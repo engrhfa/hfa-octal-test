5b6a2059b7c9ee1d606268adae2416d605291cba
HFA <hfalvarado@outlook.com>
2024-08-15 22:38:42 +0800

    Apply String Manipulation

--- election-voting-results/src/pages/ElectionResultsPage.js
+++ election-voting-results/src/pages/ElectionResultsPage.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   CandidateCard,
   ItemCard,
   ResultsCard,
 } from "../components/CardComponents";
-import { Grid, Tab, Tabs, Box, Typography, Chip } from "@mui/material";
+import { Grid, Tab, Tabs, Box, Typography, Chip, Button } from "@mui/material";
 import results from "../results.json";
 import * as helper from "../utils/helper";
 
@@ -29,41 +29,127 @@ function TabPanel(props) {
 }
 
 function ElectionResultsPage() {
-  const [value, setValue] = React.useState(0);
-  let resultsByPosition = helper.getAllResults();
+  const [value, setValue] = useState(0);
+  const [presidentHeader, setPresidentHeader] = useState("President");
+  const [vpHeader, setVpHeader] = useState("Vice President");
+  const [pageHeader, setPageHeader] = useState("ELECTION VOTING RESULTS 2022");
+  const [tabTitles, setTabTitles] = useState([
+    "Cities",
+    "Provinces",
+    "Regions",
+  ]);
+  const [resultsByPosition, setResultsByPosition] = useState(
+    helper.getAllResults()
+  );
+  const [reversedResults, setReversedResults] = useState(results);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+  const reverseStringFromCenter = (str) => {
+    const length = str.length;
+    if (length <= 1) return str;
+
+    const center = Math.floor(length / 2);
+    const beforeCenter = str.substring(0, center);
+    const afterCenter = str.substring(center);
+
+    return (
+      beforeCenter.split("").reverse().join("") +
+      afterCenter.split("").reverse().join("")
+    );
+  };
+
+  const reverseVoteString = (str) => {
+    return str
+      .split(",")
+      .map((part) => part.split("").reverse().join(""))
+      .reverse()
+      .join(",");
+  };
+
+  const handleReverseClick = () => {
+    // Reverse the resultsByPosition data
+    const reversedResultsData = { ...resultsByPosition };
+    Object.keys(reversedResultsData).forEach((position) => {
+      reversedResultsData[position].totalVotes = reversedResultsData[
+        position
+      ].totalVotes.map((item) => ({
+        ...item,
+        candidate: reverseStringFromCenter(item.candidate),
+        votes: reverseVoteString(item.votes.toString()),
+      }));
+    });
+
+    // Reverse headers, tab titles, and chips labels
+    setResultsByPosition(reversedResultsData);
+    setPresidentHeader(reverseStringFromCenter(presidentHeader));
+    setVpHeader(reverseStringFromCenter(vpHeader));
+    setPageHeader(reverseStringFromCenter(pageHeader));
+    setTabTitles(tabTitles.map((title) => reverseStringFromCenter(title)));
+
+    // Reverse the labels of cities, provinces, and regions
+    const reversedResultsCopy = {
+      cities: reversedResults.cities.map((item) => ({
+        ...item,
+        name: reverseStringFromCenter(item.name),
+      })),
+      provinces: reversedResults.provinces.map((item) => ({
+        ...item,
+        name: reverseStringFromCenter(item.name),
+      })),
+      regions: reversedResults.regions.map((item) => ({
+        ...item,
+        name: reverseStringFromCenter(item.name),
+      })),
+    };
+
+    setReversedResults(reversedResultsCopy);
+  };
 
   return (
     <>
-    <Grid
-      container
-      direction="row"
-      display={"flex"}
-      alignItems={"center"}
-      justifyContent={"center"}
-      sx={{
-        minHeight: "20vh",
-        padding: 2,
-        pt: 2,
-        backgroundColor: "#1f3476",
-        color: "#fff",
-      }}
-    >
-      <p
-        style={{
-          padding: "0px",
-          margin: "0px",
+      <Button
+        variant="contained"
+        sx={{
+          position: "absolute",
+          right: "2%",
+          top: "2%",
+          borderRadius: "20px",
+          backgroundColor: "#fff",
+          color: "#1f3476",
           textTransform: "uppercase",
-          fontWeight: "bold",
-          fontSize: "40px",
+        }}
+        onClick={handleReverseClick}
+      >
+        <b>Reverse</b>
+      </Button>
+      <Grid
+        container
+        direction="row"
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        sx={{
+          minHeight: "20vh",
+          padding: 2,
+          pt: 2,
+          backgroundColor: "#1f3476",
+          color: "#fff",
         }}
       >
-        ELECTION VOTING RESULTS 2022
-      </p>
-    </Grid>
+        <p
+          style={{
+            padding: "0px",
+            margin: "0px",
+            textTransform: "uppercase",
+            fontWeight: "bold",
+            fontSize: "40px",
+          }}
+        >
+          {pageHeader}
+        </p>
+      </Grid>
       <Grid
         container
         direction="row"
@@ -87,9 +173,9 @@ function ElectionResultsPage() {
             margin: "0 !important",
           }}
         >
-          <ResultsCard title={"President"}>
-            {resultsByPosition.President.totalVotes.map((item) => {
-              return <CandidateCard {...item} />;
+          <ResultsCard title={presidentHeader}>
+            {resultsByPosition?.President?.totalVotes?.map((item, index) => {
+              return <CandidateCard key={index} {...item} />;
             })}
           </ResultsCard>
         </Grid>
@@ -105,10 +191,12 @@ function ElectionResultsPage() {
             justifyContent: "center",
           }}
         >
-          <ResultsCard title={"Vice President"}>
-            {resultsByPosition["Vice President"].totalVotes.map((item) => {
-              return <CandidateCard {...item} />;
-            })}
+          <ResultsCard title={vpHeader}>
+            {resultsByPosition["Vice President"]?.totalVotes?.map(
+              (item, index) => {
+                return <CandidateCard key={index} {...item} />;
+              }
+            )}
           </ResultsCard>
         </Grid>
       </Grid>
@@ -124,7 +212,19 @@ function ElectionResultsPage() {
             onChange={handleChange}
             centered
           >
-            <Tab
+            {tabTitles.map((title, index) => (
+              <Tab
+                key={index}
+                label={title}
+                style={{
+                  fontFamily: "Segoe UI",
+                  fontSize: "30px",
+                  color: "#000",
+                  fontWeight: "bold",
+                }}
+              />
+            ))}
+            {/* <Tab
               label="Cities"
               style={{
                 fontFamily: "Segoe UI",
@@ -150,7 +250,7 @@ function ElectionResultsPage() {
                 color: "#000",
                 fontWeight: "bold",
               }}
-            />
+            /> */}
           </Tabs>
           <TabPanel
             id="cities"
@@ -163,9 +263,10 @@ function ElectionResultsPage() {
               justifyContent: "center !important",
             }}
           >
-            {results.cities.map((item) => {
+            {reversedResults.cities.map((item, index) => {
               return (
                 <Chip
+                  key={index}
                   label={item.name}
                   component="a"
                   href={`/cities/${item.id}`}
@@ -187,12 +288,13 @@ function ElectionResultsPage() {
             })}
           </TabPanel>
           <TabPanel id="provinces" value={value} index={1}>
-            {results.provinces.map((item) => {
+            {reversedResults.provinces.map((item, index) => {
               return (
                 <Chip
+                  key={index}
                   label={item.name}
                   component="a"
-                  href={`/province/${item.id}`}
+                  href={`/cities/${item.id}`}
                   clickable
                   style={{
                     fontSize: "20px",
@@ -211,12 +313,13 @@ function ElectionResultsPage() {
             })}
           </TabPanel>
           <TabPanel id="regions" value={value} index={2}>
-            {results.regions.map((item) => {
+            {reversedResults.regions.map((item, index) => {
               return (
                 <Chip
+                  key={index}
                   label={item.name}
                   component="a"
-                  href={`/region/${item.id}`}
+                  href={`/cities/${item.id}`}
                   clickable
                   style={{
                     fontSize: "20px",
